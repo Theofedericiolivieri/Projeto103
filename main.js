58ec1b84056951811cf17bc34bb8a4b15dcc8b6e
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2023-01-18 19:06:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,30 @@ function take_snapshot(){
     });
 }
 console.log('ml5 version:', ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/2wZ4tMQsK/model.json",modelLoaded);
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+function check(){
+    img=document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        percent=results[0].confidence.toFixed(3)*100;
+        console.log(percent);
+        document.getElementById("result_object_accuracy").innerHTML= percent + "%";
+        object=results[0].label;
+        speak();
+    }
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data= "Tenho "+percent+" porcento de confiança que este objeto é um(a) "+object;
+    var utterThis= new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis); 
+}
